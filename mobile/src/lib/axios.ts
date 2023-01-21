994893b5c0279637167ba150c91d64c0fb1d3f31
CasemiroSJunior <lordofgamer@outlook.com>
2023-01-21 17:53:20 -0300

    fix: troca de ip de lib/axios para estar de acordo com o README

--- mobile/src/lib/axios.ts
+++ mobile/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://192.168.0.189:3107' 
+    baseURL: 'http://123.456.789.10:3107' 
     /* 
     baseURL: 'http://168.195.220.75:3107'
     baseURL: 'http://26.25.206.100:3107' 
